da93c2c163dd62db07614ff343e5fc910ffec808
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-11-22 10:53:09 +0000

    temporarity removed apt

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-sudo apt-get update
-sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
-
 wget https://github.com/aishalih/sfwin/archive/refs/heads/master.zip
 sudo unzip master.zip -d /usr/share/fonts/
 sudo fc-cache -f -v
